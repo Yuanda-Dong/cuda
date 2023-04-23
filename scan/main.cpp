2237bf012999ccc690b6a24fdb0f2c491a53a59a
Yuanda Dong <dongyuanda@gmail.com>
2023-04-24 00:12:18 +1000

    djsicojc

--- scan/main.cpp
+++ scan/main.cpp
@@ -1,104 +1,95 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <getopt.h>
-#include <string>
 #include <cstring>
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
 
 #include "CycleTimer.h"
 
-double cudaFindRepeats(int *input, int length, int *output, int *output_length); 
-double cudaScan(int* start, int* end, int* resultarray);
-double cudaScanThrust(int* start, int* end, int* resultarray);
+double cudaFindRepeats(int *input, int length, int *output, int *output_length);
+double cudaScan(int *start, int *end, int *resultarray);
+double cudaScanThrust(int *start, int *end, int *resultarray);
 void printCudaInfo();
 
-
-void usage(const char* progname) {
+void usage(const char *progname) {
     printf("Usage: %s [options] \n", progname);
     printf("Program Options:\n");
-    printf("  -m  --test <TYPE>      Run specified function on input.  Valid tests are: scan, find_repeats (default: scan)\n"); 
+    printf("  -m  --test <TYPE>      Run specified function on input.  Valid tests are: scan, find_repeats (default: scan)\n");
     printf("  -i  --input <NAME>     Run test on given input type. Valid inputs are: ones, random (default: random)\n");
     printf("  -n  --arraysize <INT>  Number of elements in arrays\n");
     printf("  -t  --thrust           Use Thrust library implementation\n");
     printf("  -?  --help             This message\n");
 }
 
-void cpu_exclusive_scan(int* start, int* end, int* output) {
+void cpu_exclusive_scan(int *start, int *end, int *output) {
 
-//#define PARALLEL 1
+// #define PARALLEL 1
 #ifdef PARALLEL
 
     // note to students: this C code can be helpful when debugging the
     // output of intermediate steps of your CUDA segmented scan.
     // Uncomment the line abbove to use it as a reference.w
-  
+
     int N = end - start;
-    memmove(output, start, N*sizeof(int));
-    
+    memmove(output, start, N * sizeof(int));
+
     // upsweep phase
-    for (int twod = 1; twod < N/2; twod*=2) {
-        int twod1 = twod*2;
-	
+    for (int twod = 1; twod < N / 2; twod *= 2) {
+        int twod1 = twod * 2;
+
         for (int i = 0; i < N; i += twod1) {
-	    output[i+twod1-1] = output[i+twod-1] + output[i+twod1-1];
+            output[i + twod1 - 1] = output[i + twod - 1] + output[i + twod1 - 1];
         }
     }
 
-    output[N-1] = 0;
+    output[N - 1] = 0;
 
     // downsweep phase
-    for (int twod = N/2; twod >= 1; twod /= 2) {
-        int twod1 = twod*2;
+    for (int twod = N / 2; twod >= 1; twod /= 2) {
+        int twod1 = twod * 2;
         for (int i = 0; i < N; i += twod1) {
-            int tmp = output[i+twod-1];
-            output[i+twod-1] = output[i+twod1-1];
-            output[i+twod1-1] = tmp + output[i+twod1-1];
+            int tmp = output[i + twod - 1];
+            output[i + twod - 1] = output[i + twod1 - 1];
+            output[i + twod1 - 1] = tmp + output[i + twod1 - 1];
         }
     }
 
-#else    
+#else
     int N = end - start;
     output[0] = 0;
     for (int i = 1; i < N; i++) {
-        output[i] = output[i-1] + start[i-1];
+        output[i] = output[i - 1] + start[i - 1];
     }
 #endif
 }
 
 int cpu_find_repeats(int *start, int length, int *output) {
     int count = 0, idx = 0;
-    while (idx < length - 1){ 
-        if (start[idx] == start[idx + 1]){
+    while (idx < length - 1) {
+        if (start[idx] == start[idx + 1]) {
             output[count] = idx;
             count++;
-        }   
+        }
         idx++;
-    }   
+    }
     return count;
 }
 
+int main(int argc, char **argv) {
 
-int main(int argc, char** argv) {
-  
     int N = 64;
     bool useThrust = false;
-    std::string test("scan"); 
+    std::string test("scan");
     std::string input("random");
 
     // parse commandline options ////////////////////////////////////////////
     int opt;
-    static struct option long_options[] = {
-        {"test",       1, 0, 'm'},
-        {"arraysize",  1, 0, 'n'},
-        {"input",      1, 0, 'i'},
-        {"help",       0, 0, '?'},
-        {"thrust",     0, 0, 't'},
-        {0 ,0, 0, 0}
-    };
+    static struct option long_options[] = {{"test", 1, 0, 'm'}, {"arraysize", 1, 0, 'n'}, {"input", 1, 0, 'i'}, {"help", 0, 0, '?'}, {"thrust", 0, 0, 't'}, {0, 0, 0, 0}};
 
     while ((opt = getopt_long(argc, argv, "m:n:i:?t", long_options, NULL)) != EOF) {
         switch (opt) {
         case 'm':
-            test = optarg; 
+            test = optarg;
             break;
         case 'n':
             N = atoi(optarg);
@@ -117,9 +108,9 @@ int main(int argc, char** argv) {
     }
     // end parsing of commandline options //////////////////////////////////////
 
-    int* inarray = new int[N];
-    int* resultarray = new int[N];
-    int* checkarray = new int[N];
+    int *inarray = new int[N];
+    int *resultarray = new int[N];
+    int *checkarray = new int[N];
 
     if (input.compare("random") == 0) {
 
@@ -131,14 +122,14 @@ int main(int argc, char** argv) {
             inarray[i] = val;
             checkarray[i] = val;
         }
-	
+
     } else {
-      
+
         // all one's test case - you may find this useful for debugging
-        for(int i = 0; i < N; i++) {
+        for (int i = 0; i < N; i++) {
             inarray[i] = 1;
             checkarray[i] = 1;
-        }  
+        }
     }
 
     printCudaInfo();
@@ -146,26 +137,26 @@ int main(int argc, char** argv) {
     double cudaTime = 50000.;
 
     printf("Array size: %d\n", N);
-    
+
     if (test.compare("scan") == 0) { // test exclusive scan
-      
+
         // run CUDA implementation
-        for (int i=0; i<1; i++) {
-      //for (int i=0; i<3; i++) {
+        for (int i = 0; i < 1; i++) {
+            // for (int i=0; i<3; i++) {
             if (useThrust)
-                cudaTime = std::min(cudaTime, cudaScanThrust(inarray, inarray+N, resultarray));
+                cudaTime = std::min(cudaTime, cudaScanThrust(inarray, inarray + N, resultarray));
             else
-                cudaTime = std::min(cudaTime, cudaScan(inarray, inarray+N, resultarray));
+                cudaTime = std::min(cudaTime, cudaScan(inarray, inarray + N, resultarray));
         }
 
         // run CPU implementation to check correctness
-        cpu_exclusive_scan(inarray, inarray+N, checkarray);
+        cpu_exclusive_scan(inarray, inarray + N, checkarray);
 
-        if (useThrust) { 
+        if (useThrust) {
             printf("Thrust GPU time: %.3f ms\n", 1000.f * cudaTime);
-        } else {    
+        } else {
             printf("Student GPU time: %.3f ms\n", 1000.f * cudaTime);
-        } 
+        }
 
         // validate results
         for (int i = 0; i < N; i++) {
@@ -178,16 +169,16 @@ int main(int argc, char** argv) {
             }
         }
         printf("Scan outputs are correct!\n");
-	
+
     } else if (test.compare("find_repeats") == 0) { // Test find_repeats
-        
+
         // run CUDA implementation
         int cu_size;
-        for (int i=0; i<3; i++) {
+        for (int i = 0; i < 3; i++) {
             cudaTime = std::min(cudaTime, cudaFindRepeats(inarray, N, resultarray, &cu_size));
         }
 
-        // run CPU implementation to check correctness 
+        // run CPU implementation to check correctness
         int serial_size = cpu_find_repeats(inarray, N, checkarray);
 
         printf("Student GPU time: %.3f ms\n", 1000.f * cudaTime);
@@ -200,26 +191,26 @@ int main(int argc, char** argv) {
                     serial_size, cu_size);
             exit(1);
         }
-	
+
         for (int i = 0; i < serial_size; i++) {
             if (checkarray[i] != resultarray[i]) {
                 fprintf(stderr,
                         "Error: Device find_repeats outputs incorrect result."
                         " A[%d] = %d, expecting %d.\n",
                         i, resultarray[i], checkarray[i]);
-                // exit(1);
+                exit(1);
             }
         }
         printf("Find_repeats outputs are correct!\n");
 
-    } else { 
-        usage(argv[0]); 
-        exit(1); 
+    } else {
+        usage(argv[0]);
+        exit(1);
     }
-    
-    delete [] inarray;
-    delete [] resultarray;
-    delete [] checkarray;
-    
+
+    delete[] inarray;
+    delete[] resultarray;
+    delete[] checkarray;
+
     return 0;
 }
